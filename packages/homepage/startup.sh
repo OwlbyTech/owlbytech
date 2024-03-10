fdab42104653fb935414edb6e48d83c29c3ad83e
camilo-camargo <camilocamargo49@gmail.com>
2024-03-10 12:10:24 -0500

    - Dockerize the whole project - Change npm to yarn berry - Add dev environment

---
+++ packages/homepage/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# TODO: Check if the script is running on docker container or outside.
+echo "Installing bash..."
+apk add bash
+
+echo "Installing dependiencies"
+yarn install
+
+if [ $ENV = "prod" ]; then
+  echo "------------ PRODUCTION MODE ------------"
+  yarn run build
+  yarn start
+else
+  echo "------------ DEVELOPMENT MODE ------------"
+  yarn run dev
+fi
